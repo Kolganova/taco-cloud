ac1b1e30770d169199edfdc71448903648bb1113
Diana Kolganova <glebova_diana@bk.ru>
2023-11-23 13:46:21 +0300

    * mod jdbc -> jpa

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,27 +1,28 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @NoArgsConstructor
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date createdAt = new Date();
     @NonNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @ManyToMany
     @NonNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients;
+    private List<Ingredient> ingredients;
 }
--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,12 +1,11 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -15,12 +14,13 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Table("Taco_Cloud_Order")
+@Entity(name = "Taco_Cloud_Order")
 public class TacoOrder implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt = new Date();
     @NotBlank(message = "Delivery name is required")
@@ -40,6 +40,7 @@ public class TacoOrder implements Serializable {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
