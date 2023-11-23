19b97119053ca4ba70257caf3a5142d0e6fa8133
Diana Kolganova <glebova_diana@bk.ru>
2023-11-24 02:33:29 +0300

    * mod in progress jpa -> cassandra (Ingredient, Taco classes done)

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,28 +1,36 @@
 package tacos;
 
-import jakarta.persistence.*;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
-@NoArgsConstructor
 @Data
-@Entity
+@Table("tacos")
 public class Taco {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    private UUID id = Uuids.timeBased();
+    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private Date createdAt = new Date();
     @NonNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
-    @ManyToMany
-    @NonNull
+    @Column("ingredients")
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientUDT> ingredients;
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(TacoUDRUtils.toIngredientUDT(ingredient));
+    }
+
 }
