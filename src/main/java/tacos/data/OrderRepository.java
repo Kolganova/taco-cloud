6e6bbe92e3048231062466a1cbba7be6e97dc635
Diana Kolganova <glebova_diana@bk.ru>
2023-11-23 14:19:57 +0300

    * add specific methods in OrderRep. delete basic methods from IngredientRep

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,10 +1,19 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
-    TacoOrder save(TacoOrder order);
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readTacoOrderByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+    List<TacoOrder> findByDeliveryNameAndDeliveryCityAllIgnoreCase(String deliveryName, String deliveryCity);
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryCity(String deliveryCity);
+    @Query("Order o WHERE o.deliveryCity = 'Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 
 }
