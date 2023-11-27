ac9f6f778a5b0ea749448ef879cc61998a61f14e
Diana Kolganova <glebova_diana@bk.ru>
2023-11-27 19:46:32 +0300

    * mod login abilities, add logout

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import tacos.User;
 import tacos.data.UserRepository;
 
@@ -25,6 +27,24 @@ public class SecurityConfig {
                 return user;
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/design", "/orders").hasRole("USER")
+                        .requestMatchers("/", "/**").permitAll())
+
+                .oauth2Login(oauth -> oauth
+                        .loginPage("/login"))
+
+                .formLogin(form ->
+                        form.loginPage("/login"))
+
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+
+                .build();
     }
 
 }
