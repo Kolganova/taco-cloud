5384a255e767c658da2b658fdc68f16d23587889
Diana Kolganova <glebova_diana@bk.ru>
2023-11-29 00:13:04 +0300

    * add rest api

---
+++ src/main/java/tacos/data/TacoRepository.java
@@ -0,0 +1,12 @@
+package tacos.data;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.CrudRepository;
+import tacos.Taco;
+
+public interface TacoRepository extends CrudRepository<Taco, Long> {
+
+    Page<Taco> findAll(PageRequest page);
+
+}
